2793f63c2df142b7b2eb39c6f733455892eaef8e
debel damilola <debeldamilola@gmail.com>
2020-09-22 14:38:31 +0100

    implemented jwt token

--- controllers/auth.js
+++ controllers/auth.js
@@ -17,5 +17,8 @@ exports.register = asyncHandler(async (req, res, next) => {
     role,
   });
 
-  res.status(200).json({ success: user });
+  //create token from static method created in user model
+  const token = user.getSignedJwtToken();
+
+  res.status(200).json({ success: true, token });
 });
