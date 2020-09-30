9bb8680aeb9cee1650cc11551d1c409e07f13cfa
debel damilola <debeldamilola@gmail.com>
2020-09-30 23:05:28 +0100

    implement update and delete review route

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -35,9 +35,7 @@ exports.getReview = asyncHandler(async (req, res, next) => {
   });
 
   if (!review) {
-    return next(
-      new ErrorResponse(`No review found with the id of ${req.params.id}`, 404)
-    );
+    return next(new ErrorResponse(`no review with id found`, 404));
   }
 
   res.status(200).json({
@@ -58,12 +56,7 @@ exports.addReview = asyncHandler(async (req, res, next) => {
   const bootcamp = await Bootcamp.findById(req.params.bootcampId);
 
   if (!bootcamp) {
-    return next(
-      new ErrorResponse(
-        `No bootcamp with the id of ${req.params.bootcampId}`,
-        404
-      )
-    );
+    return next(new ErrorResponse(`No bootcamp with the id found`, 404));
   }
 
   const review = await Review.create(req.body);
@@ -71,5 +64,60 @@ exports.addReview = asyncHandler(async (req, res, next) => {
   res.status(201).json({
     success: true,
     data: review,
+  });
+});
+
+/**
+ * @desc      Update review
+ * @route     PUT /api/v1/reviews/:id
+ * @access    Private
+ */
+exports.updateReview = asyncHandler(async (req, res, next) => {
+  let review = await Review.findById(req.params.id);
+
+  if (!review) {
+    return next(new ErrorResponse(`no review with id found`, 404));
+  }
+
+  // Make sure review belongs to user or user is admin
+  if (review.user.toString() !== req.user.id && req.user.role !== 'admin') {
+    return next(new ErrorResponse(`Not authorized to update review`, 401));
+  }
+
+  review = await Review.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+
+  review.save();
+
+  res.status(200).json({
+    success: true,
+    data: review,
+  });
+});
+
+/**
+ * @desc      Delete review
+ * @route     DELETE /api/v1/reviews/:id
+ * @access    Private
+ */
+exports.deleteReview = asyncHandler(async (req, res, next) => {
+  const review = await Review.findById(req.params.id);
+
+  if (!review) {
+    return next(new ErrorResponse(`no review with id found`, 404));
+  }
+
+  // Make sure review belongs to user or user is admin
+  if (review.user.toString() !== req.user.id && req.user.role !== 'admin') {
+    return next(new ErrorResponse(`Not authorized to update review`, 401));
+  }
+
+  await review.remove();
+
+  res.status(200).json({
+    success: true,
+    data: {},
   });
 });
